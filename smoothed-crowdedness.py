b68ead4943a3037d16584850ac714a1461d63c29
Sebastiano Chiari <sebastiano.chiari@studenti.unitn.it>
2020-02-21 11:17:33 +0100

    modified smoothed-crowdedness.py + computed all the smoothed-crowdedness from all the dataset per each radius

--- smoothed-crowdedness.py
+++ smoothed-crowdedness.py
@@ -11,6 +11,98 @@ print('loading python modules')
 # load pandas library as pd
 import pandas as pd
 
+# function to compute the smoothed-crowdedness
+def computeSmoothCrowdedness(r):
+    # set radius e smooth radius
+    radius = r
+
+    if radius == 1:
+        loc_crowd = 5
+        loc_smooth = 8
+    elif radius == 2:
+        loc_crowd = 6
+        loc_smooth = 9
+    elif radius == 5:
+        loc_crowd = 7
+        loc_smooth = 10
+
+
+    for x in range(offset_value,columns - offset_value):
+        
+        # saving th crowdedness from the frame I'm looking
+        media = data.iloc[loc_crowd,x]
+        media_divisor = 1
+
+        for j in range(1,offset_value+1):
+            offset = x + j
+            if data.iloc[0,x] == data.iloc[0,offset]:
+                media = media + data.iloc[loc_crowd,offset]
+                media_divisor = media_divisor + 1
+            offset = x - j
+            if data.iloc[0,x] == data.iloc[0,offset]:
+                media = media + data.iloc[loc_crowd,offset]
+                media_divisor = media_divisor + 1
+        
+        media = float(media / media_divisor)
+        data.iloc[loc_smooth, x] = int(round(media))
+
+    # edit the left out values
+    x = 0
+    media = data.iloc[loc_crowd, x]
+    media_divisor = 1
+    for j in range(1, offset_value+1):
+        offset = x + j
+        if data.iloc[0,x] == data.iloc[0,offset]:
+            media = media + data.iloc[loc_crowd,offset]
+            media_divisor = media_divisor + 1
+    media = float(media / media_divisor)
+    data.iloc[loc_smooth, x] = int(round(media))
+
+    x = 1
+    media = data.iloc[loc_crowd, x]
+    media_divisor = 1
+    for j in range(1, offset_value+1):
+        offset = x + j
+        if data.iloc[0,x] == data.iloc[0,offset]:
+            media = media + data.iloc[loc_crowd,offset]
+            media_divisor = media_divisor + 1
+        offset = x - j
+        if j <= x:
+            if data.iloc[0,x] == data.iloc[0,offset]:
+                media = media + data.iloc[loc_crowd,offset]
+                media_divisor = media_divisor + 1
+
+    media = float(media / media_divisor)
+    data.iloc[loc_smooth, x] = int(round(media))
+
+    x = columns - 2
+    media = data.iloc[loc_crowd, x]
+    media_divisor = 1
+    for j in range(1, offset_value+1):
+        offset = x + j
+        if j == 1:
+            if data.iloc[0,x] == data.iloc[0,offset]:
+                media = media + data.iloc[loc_crowd,offset]
+                media_divisor = media_divisor + 1
+        offset = x - j
+        if data.iloc[0,x] == data.iloc[0,offset]:
+            media = media + data.iloc[loc_crowd,offset]
+            media_divisor = media_divisor + 1
+
+    media = float(media / media_divisor)
+    data.iloc[loc_smooth, x] = int(round(media))
+
+    x = columns - 1
+    media = data.iloc[loc_crowd, x]
+    media_divisor = 1
+    for j in range(1, offset_value+1):
+        offset = x - j
+        if data.iloc[0,x] == data.iloc[0,offset]:
+            media = media + data.iloc[loc_crowd,offset]
+            media_divisor = media_divisor + 1
+    media = float(media / media_divisor)
+    data.iloc[loc_smooth, x] = int(round(media))
+
 # importing dataset
 file = "crowded_real_video_trajectory/ucy_zara02.csv"
 data = pd.read_csv(file, sep=",", header=None)
@@ -38,95 +130,14 @@ offset_value = int(smoothing_window/2)
 # retrieve the number of columns from the dataframe
 columns = (data.shape)[1]
 
-# set radius e smooth radius
-radius = 5
+print('computing smooth-crowdedness radius 1')
+computeSmoothCrowdedness(1)
 
-if radius == 1:
-    loc_crowd = 5
-    loc_smooth = 8
-elif radius == 2:
-    loc_crowd = 6
-    loc_smooth = 9
-elif radius == 5:
-    loc_crowd = 7
-    loc_smooth = 10
+print('computing smooth-crowdedness radius 2')
+computeSmoothCrowdedness(2)
 
-
-for x in range(offset_value,columns - offset_value):
-    
-    # saving th crowdedness from the frame I'm looking
-    media = data.iloc[loc_crowd,x]
-    media_divisor = 1
-
-    for j in range(1,offset_value+1):
-        offset = x + j
-        if data.iloc[0,x] == data.iloc[0,offset]:
-            media = media + data.iloc[loc_crowd,offset]
-            media_divisor = media_divisor + 1
-        offset = x - j
-        if data.iloc[0,x] == data.iloc[0,offset]:
-            media = media + data.iloc[loc_crowd,offset]
-            media_divisor = media_divisor + 1
-    
-    media = float(media / media_divisor)
-    data.iloc[loc_smooth, x] = int(round(media))
-
-# edit the left out values
-x = 0
-media = data.iloc[loc_crowd, x]
-media_divisor = 1
-for j in range(1, offset_value+1):
-    offset = x + j
-    if data.iloc[0,x] == data.iloc[0,offset]:
-        media = media + data.iloc[loc_crowd,offset]
-        media_divisor = media_divisor + 1
-media = float(media / media_divisor)
-data.iloc[loc_smooth, x] = int(round(media))
-
-x = 1
-media = data.iloc[loc_crowd, x]
-media_divisor = 1
-for j in range(1, offset_value+1):
-    offset = x + j
-    if data.iloc[0,x] == data.iloc[0,offset]:
-        media = media + data.iloc[loc_crowd,offset]
-        media_divisor = media_divisor + 1
-    offset = x - j
-    if j <= x:
-        if data.iloc[0,x] == data.iloc[0,offset]:
-            media = media + data.iloc[loc_crowd,offset]
-            media_divisor = media_divisor + 1
-
-media = float(media / media_divisor)
-data.iloc[loc_smooth, x] = int(round(media))
-
-x = columns - 2
-media = data.iloc[loc_crowd, x]
-media_divisor = 1
-for j in range(1, offset_value+1):
-    offset = x + j
-    if j == 1:
-        if data.iloc[0,x] == data.iloc[0,offset]:
-            media = media + data.iloc[loc_crowd,offset]
-            media_divisor = media_divisor + 1
-    offset = x - j
-    if data.iloc[0,x] == data.iloc[0,offset]:
-        media = media + data.iloc[loc_crowd,offset]
-        media_divisor = media_divisor + 1
-
-media = float(media / media_divisor)
-data.iloc[loc_smooth, x] = int(round(media))
-
-x = columns - 1
-media = data.iloc[loc_crowd, x]
-media_divisor = 1
-for j in range(1, offset_value+1):
-    offset = x - j
-    if data.iloc[0,x] == data.iloc[0,offset]:
-        media = media + data.iloc[loc_crowd,offset]
-        media_divisor = media_divisor + 1
-media = float(media / media_divisor)
-data.iloc[loc_smooth, x] = int(round(media))
+print('computing smooth-crowdedness radius 5')
+computeSmoothCrowdedness(5)
 
 # reset the matrix to its original form-factor (frames in the first row and ordered by frame)
 df_transposed = data.transpose()
